ad71820d0466f881bad3a02f2f15f6f98ae7fe17
Sameer Puri <sameer@users.noreply.github.com>
2020-06-06 20:54:21 -0400

    write image to /dev/shm instead of userspace

--- forever.sh
+++ forever.sh
@@ -1,7 +1,7 @@
 while :
 do
     cargo run
-    sshpass -p root scp out.png 192.168.2.2:/mnt/us/
-    sshpass -p root ssh 192.168.2.2 /usr/sbin/eips -g /mnt/us/out.png
+    sshpass -p root scp out.png 192.168.2.2:/dev/shm/
+    sshpass -p root ssh 192.168.2.2 /usr/sbin/eips -g /dev/shm/out.png
     sleep 55s
 done
