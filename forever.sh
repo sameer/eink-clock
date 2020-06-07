c0611257339c78230bbabf069ed54cb455a18883
Sameer Puri <sameer@users.noreply.github.com>
2020-06-06 20:45:25 -0400

    reduce sleep time

--- forever.sh
+++ forever.sh
@@ -1,4 +1,7 @@
 while :
 do
-    cargo run && sshpass -p root scp out.png 192.168.2.2:/mnt/us/ && sshpass -p root ssh 192.168.2.2 /usr/sbin/eips -g /mnt/us/out.png && sleep 58s
+    cargo run
+    sshpass -p root scp out.png 192.168.2.2:/mnt/us/
+    sshpass -p root ssh 192.168.2.2 /usr/sbin/eips -g /mnt/us/out.png
+    sleep 55s
 done
