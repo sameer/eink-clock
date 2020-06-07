c2e5ab073d61d9aac191a689e164e864989d6abc
Sameer Puri <sameer@users.noreply.github.com>
2020-06-07 17:45:20 -0400

    migrate to svg rendering

---
+++ README.md
@@ -0,0 +1,99 @@
+# eink-clock
+
+A clock for e-ink displays like that in the [Kindle DX Graphite](https://en.wikipedia.org/wiki/Amazon_Kindle#Kindle_DX_Graphite).
+
+## Setup
+
+### Physical requirements
+
+* Raspberry Pi Zero W or other ARM device of your choice with networking capabilities
+* [jailbroken Kindle](https://wiki.mobileread.com/wiki/Kindle_Hacks_Information#Jail_break_JB) with usbnetwork
+
+
+### Software
+
+#### Prerequisites
+
+* [Rust Language](https://www.rust-lang.org/learn/get-started) and [Cargo package manager](https://doc.rust-lang.org/cargo/index.html)
+
+#### Building
+
+##### Build on your computer (cross-compile)
+
+###### ArchLinux
+
+Get the cross-compile toolchain:
+```bash
+yay -S arm-linux-gnueabihf-gcc-linaro-bin
+```
+
+Build
+```bash
+HARFBUZZ_SYS_NO_PKG_CONFIG=1 cargo build --release
+```
+
+##### Building on an ARM device
+
+First get the harfbuzz library, used for text rendering:
+###### ArchLinux
+```bash
+pacman -S harfbuzz
+```
+
+Now build.
+```bash
+# May take a long time
+cargo build --release
+```
+
+#### Deploying
+
+If you cross-compiled, transfer the project folder to the device.
+
+##### Set up Kindle networking
+
+To network with a Kindle that has usbnetwork enabled and shows up as usb0 in `ip addr`:
+
+Create a static profile for any usb network gadgets at the end of `/etc/dhcpd.conf`:
+```
+interface usb*
+static ip_address=192.168.2.1/24
+static routers=192.168.2.1
+```
+
+If you are connecting multiple Kindles, you'll need to change the usbnetwork config to assign unique MAC addresses and unique IP addresses.
+
+If you haven't already, enable dhcpcd on startup so the Kindle network interface can be configured on startup and hotplug.
+```bash
+systemctl enable dhcpcd
+# Make sure to disable interface-specific targets like wlan0, if needed
+systemctl disable dhcpcd@wlan0
+reboot
+```
+
+It will hang on the start command if it's set up incorrectly.
+Now, the device can network with the Kindle.
+
+On the Kindle, make sure you've enabled auto-start for usbnetwork just in case the Kindle loses power:
+
+```bash
+ssh root@192.168.2.2
+ls /mnt/us/usbnet/
+# If there is a file DISABLED_auto, rename it to auto
+# Beware that this means networking will always be enabled at startup
+# You cannot connect the Kindle as a USB storage device again until you rename auto to DISABLED_auto
+mv /mnt/us/usbnet/DISABLED_auto /mnt/us/usbnet/auto
+```
+
+##### Set up systemd files
+
+These are needed to run eink-clock once at the start of every minute.
+
+```bash
+ln -s /root/eink-clock/eink-clock.service /etc/systemd/system/
+ln -s /root/eink-clock/eink-clock.timer /etc/systemd/system/
+systemctl enable eink-clock.timer
+systemctl start eink-clock.service
+```
+
+And that's it, the clock should now be running! Feel free to [contact me](https://purisa.me/about/) if you have problems.
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,46 @@
-use std::fs::File;
-use std::io::Write;
+use std::path::PathBuf;
 
-use cairo::{Context, FontFace, FontOptions, FontSlant, FontWeight, Format, ImageSurface};
+use resvg::prelude::*;
 use chrono::prelude::*;
 
-const WIDTH: f64 = 1200.0;
-const HEIGHT: f64 = 824.0;
+/// E Ink Pearl 1200x824 150 DPI 4-bit 16-level grayscale
+const WIDTH: u32 = 1200;
+const HEIGHT: u32 = 824;
+const DPI: f64 = 150.0;
+
+const LANG: &str = "en-US";
 const FONT: &str = "Inter";
 
 fn main() {
-    let surf = ImageSurface::create(Format::A8, HEIGHT as i32, WIDTH as i32)
-        .expect("couldn't create Cairo surface");
-    let ctx = Context::new(&surf);
-    let font = FontFace::toy_create(FONT, FontSlant::Normal, FontWeight::Normal);
-    let font_opts = FontOptions::default();
-    ctx.set_font_face(&font);
-    ctx.set_font_options(&font_opts);
-    ctx.set_font_size(96.);
-    ctx.set_source_rgb(1., 1., 1.);
-    ctx.translate(HEIGHT / 2., WIDTH / 2.);
-    ctx.rotate(-90.0 * std::f64::consts::PI / 180.0);
+    let svg_text = get_svg_text();
+    let mut image = render(&svg_text).expect("failed to render SVG");
+    let path = PathBuf::from("out.png");
+    image.save_png(&path);
+}
 
-    let date = get_date_as_string();
-    let extent = ctx.text_extents(&date);
-    println!("{:?}", extent);
-    ctx.move_to(-WIDTH/2., -HEIGHT/2. + extent.height);
-    ctx.show_text(&date);
-    println!("{:?}", ctx.get_current_point());
-    ctx.rel_move_to(-extent.x_advance, 0.0);
-    println!("{:?}", ctx.get_current_point());
+fn render(svg_text: &str)  -> Result<Box<dyn OutputImage>, usvg::Error> {
+    let mut options = resvg::Options::default();
+    options.fit_to = FitTo::Width(WIDTH);
+    options.usvg.dpi = DPI;
+    options.usvg.font_family = FONT.to_owned();
+    options.usvg.font_size = DPI;
+    options.usvg.languages = vec![LANG.to_owned()];
+    options.usvg.shape_rendering = usvg::ShapeRendering::CrispEdges;
+    options.usvg.text_rendering = usvg::TextRendering::OptimizeLegibility;
+    options.usvg.image_rendering = usvg::ImageRendering::OptimizeQuality;
 
-    let time = get_time_as_string();
-    let extent = ctx.text_extents(&time);
-    println!("{:?}", extent);
-    ctx.rel_move_to(0.0, 96.0);
-    println!("{:?}", ctx.get_current_point());
-    ctx.show_text(&time);
+    let tree = usvg::Tree::from_str(svg_text, &options.usvg)?;
+    let backend = Box::new(resvg::backend_raqote::Backend);
+    Ok(backend.render_to_image(&tree, &options).expect("couldn't allocate image in raqote backend"))
+}
 
-    let mut out_png = File::create("out.png").expect("couldn't create output file");
-    surf.write_to_png(&mut out_png).unwrap();
+fn get_svg_text() -> String {
+    format!(r#"
+    <svg viewBox="0 0 {width} {height}" width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg">
+        <text font-size="0.75in" x="50%" y="0.75in" text-anchor="middle">{date}</text>
+        <text font-size="1.5in" x="50%" y="95%" text-anchor="middle">{time}</text>
+    </svg>
+    "#, width=WIDTH, height=HEIGHT, date=get_date_as_string(), time=get_time_as_string())
 }
 
 fn get_date_as_string() -> String {
