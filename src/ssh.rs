9eeea0914dd1c7db3ffb3ac7b33f124c37f01338
Sameer Puri <sameer@users.noreply.github.com>
2020-09-09 13:14:48 -0400

    consume io like ssh2 tests

--- src/ssh.rs
+++ src/ssh.rs
@@ -2,7 +2,7 @@ use crate::{
     KINDLE_CONNECT_TIMEOUT, KINDLE_IP_ADDRESS, KINDLE_PASSWORD, KINDLE_SSH_PORT, KINDLE_USERNAME,
 };
 
-use ssh2::Session;
+use ssh2::{Session, Channel};
 use std::io::Write;
 use std::net::{SocketAddr, TcpStream};
 use std::path::Path;
@@ -11,6 +11,17 @@ use std::time::Duration;
 pub fn open_tcp_connection() -> std::io::Result<TcpStream> {
     let address = SocketAddr::new(KINDLE_IP_ADDRESS, KINDLE_SSH_PORT);
     TcpStream::connect_timeout(&address, Duration::from_millis(KINDLE_CONNECT_TIMEOUT))
+}
+
+fn consume_io(channel: &mut Channel) -> std::io::Result<()> {
+    use std::io::Read;
+    for b in channel.bytes() {
+        b?;
+    }
+    for b in channel.stderr().bytes() {
+        b?;
+    }
+    Ok(())
 }
 
 pub fn open_ssh_session(tcp_stream: TcpStream) -> Result<Session, ssh2::Error> {
@@ -39,8 +50,9 @@ pub fn eips_show_image(
     };
     debug!("{}", cmd);
     channel.exec(cmd)?;
-    channel.close()?;
+    consume_io(&mut channel).unwrap();
     channel.wait_eof()?;
+    channel.close()?;
     channel.wait_close()
 }
 
@@ -49,8 +61,9 @@ pub fn amixer_set_master_volume(session: &mut Session, volume: u8) -> Result<(),
     let cmd = format!("/usr/bin/amixer set Master {}%", volume);
     debug!("{}", cmd);
     channel.exec(&cmd)?;
-    channel.close()?;
+    consume_io(&mut channel).unwrap();
     channel.wait_eof()?;
+    channel.close()?;
     channel.wait_close()
 }
 
@@ -64,7 +77,8 @@ pub fn aplay_audio_nonblocking(session: &mut Session, audio: &[u8]) -> Result<()
     let cmd = "/usr/bin/aplay -q -N /dev/shm/out.wav";
     debug!("{}", cmd);
     channel.exec(cmd)?;
-    channel.close()?;
+    consume_io(&mut channel).unwrap();
     channel.wait_eof()?;
+    channel.close()?;
     channel.wait_close()
 }
